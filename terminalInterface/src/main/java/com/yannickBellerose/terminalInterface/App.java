7973681ad791382f85d658c265c58617d0886a15
Yannick Bellerose <yan-bel@hotmail.com>
2020-11-26 14:22:30 -0500

    plan de travail

--- terminalInterface/src/main/java/com/yannickBellerose/terminalInterface/App.java
+++ terminalInterface/src/main/java/com/yannickBellerose/terminalInterface/App.java
@@ -1,13 +1,25 @@
 package com.yannickBellerose.terminalInterface;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//On ajoute les elements a root dans new Group(element 1, element 2, etc.)
+		Group root = new Group();
+		Scene scene = new Scene(root, 600, 300);
+		
+		primaryStage.setTitle("K.I.S.S.I.T.");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch( args );
     }
 }
